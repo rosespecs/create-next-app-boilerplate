e0badbdcf5d791702623997c2fb06c3d97ca304a
Rose Specs <rosespecs@rosebook.local>
2021-06-14 16:31:31 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Boilerplate NextJS app 🚀
+
+Slimed down create next js app.
+[Find the create-next-app here](https://nextjs.org/docs/api-reference/create-next-app)
+
+## On top of create-next-app this boilerplate includes
+
+## Why?
+I do the same thing every time I start a NextJS project therefore, it's time for a boilerplate!
---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Default app</title>
+        <meta name="description" content="Slimmed down create NextJS app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main></main>
+      <footer></footer>
+    </>
+  );
+}
